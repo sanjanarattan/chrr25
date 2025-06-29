6512b174e8c4c150f8b34bbc0f9d18f99cd0ba0b
Sanjana Rattan <sprasad10@dons.usfca.edu>
2025-06-29 15:45:06 -0700

    Color coding/ adding more health measures

--- maps/controls.js
+++ maps/controls.js
@@ -1,23 +1,24 @@
-const mapType = document.getElementById("mapType");
-const filterSelect = document.getElementById("filterSelect");
-
+const mapType = document.getElementById("mapType")
+const filterSelect = document.getElementById("filterSelect")
+const infoType = document.getElementById("infoType")
 
 mapType.addEventListener('change', function() {
-  if (this.checked) {
-    drawCounties()
-    colorCounties(filterSelect.value)
-  } else {
-    drawStates()
-    colorStates(filterSelect.value)
-  }
+    if (this.checked) {
+        drawCounties()
+        colorCounties(filterSelect.value)
+    } else {
+        drawStates()
+        colorStates(filterSelect.value)
+    }
 });
 
 filterSelect.addEventListener('change', function() {
-  if (mapType.checked) {
-    drawCounties()
-    colorCounties(filterSelect.value)
-  } else {
-    drawStates()
-    colorStates(filterSelect.value)
-  }
+    if (mapType.checked) {
+        drawCounties()
+        colorCounties(filterSelect.value)
+    } else {
+        drawStates()
+        colorStates(filterSelect.value)
+    }
+
 });
